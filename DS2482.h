3fa3f579993b157a4ae378b762511b66f498d7d6
Pavel Slama <p.slama@atlas.cz>
2019-04-24 10:32:33 +0200

    remove debug

--- DS2482.h
+++ DS2482.h
@@ -62,19 +62,6 @@ SOFTWARE.
 #define DS2482_STATUS_TSB  (1<<6)
 #define DS2482_STATUS_DIR  (1<<7)
 
-
-
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte)  \
-  (byte & 0x80 ? '1' : '0'), \
-  (byte & 0x40 ? '1' : '0'), \
-  (byte & 0x20 ? '1' : '0'), \
-  (byte & 0x10 ? '1' : '0'), \
-  (byte & 0x08 ? '1' : '0'), \
-  (byte & 0x04 ? '1' : '0'), \
-  (byte & 0x02 ? '1' : '0'), \
-  (byte & 0x01 ? '1' : '0')
-
 class DS2482 {
  public:
   typedef enum {
